7b6116b8d18a759d37adcc3ede783fad8a7a2d1c
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2023-10-12 18:29:25 -0300

    fix: now using PostgreSQL ignoring sqlite3

--- blog_api/settings/base.py
+++ blog_api/settings/base.py
@@ -100,14 +100,14 @@ WSGI_APPLICATION = "blog_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ROOT_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": ROOT_DIR / "db.sqlite3",
+#     }
+# }
 
-# DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {"default": env.db("DATABASE_URL")}
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.Argon2PasswordHasher",
